81ecb2e7d69d70c3dc505280f2442c0e32ba4a6f
ander <ander.zubizarreta@ikasle.aeg.eus>
2025-01-15 12:29:56 +0100

    [NEW]advanced details modal

---
+++ src/components/DetallesAvanzados.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Potion } from '../types/Potion';
+
+interface DetallesAvanzadosProps {
+  potion: Potion;
+}
+
+const DetallesAvanzados: React.FC<DetallesAvanzadosProps> = ({ potion }) => {
+  return (
+    <div className="p-4 overflow-x-auto">
+      <div className="flex space-x-4 min-w-max">
+            <p className="text-lg text-gray-950">Hola</p>
+          
+          </div>
+        
+      </div>
+  );
+};
+
+export default DetallesAvanzados;
